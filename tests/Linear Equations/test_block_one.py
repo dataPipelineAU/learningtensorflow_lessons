e2ab25bbbe8244cb6a58e22c2c7ff050de6112e7
Matt <deal.sy@hotmail.com>
2017-04-21 12:44:54 +1000

    updated 3d code and linear code

--- tests/Linear Equations/test_block_one.py
+++ tests/Linear Equations/test_block_one.py
@@ -3,12 +3,12 @@ import tensorflow as tf
 # Point 1
 x1 = tf.constant(2, dtype=tf.float32)
 y1 = tf.constant(1, dtype=tf.float32)
-point1 = tf.pack([x1, y1])
+point1 = tf.stack([x1, y1])
 
 # Point 2
 x2 = tf.constant(-1, dtype=tf.float32)
 y2 = tf.constant(1, dtype=tf.float32)
-point2 = tf.pack([x2, y2])
+point2 = tf.stack([x2, y2])
 
 # Combine points into an array
 X = tf.transpose(tf.pack([point1, point2]))
