82fd167cfe37c8e4af09270107c81779bc1b9595
dealsy <deal.sy@hotmail.com>
2017-06-02 13:58:43 +1000

    removed bad code from 3d

--- tests/Tensorflow_in_3D/test_3D_one.py
+++ tests/Tensorflow_in_3D/test_3D_one.py
@@ -5,13 +5,7 @@ import matplotlib.pyplot as plt
 from scipy.spatial import Delaunay
 
 
-#!python3
-#coding: utf8
-symbols = '.'
-D = str.maketrans('','',symbols)
-with open('All12.txt','a',encoding='utf8') as t, open('All11.txt','r',encoding='utf8') as n:
-    for line in n:
-        t.write(line.translate(D))
+
 
 def plot_basic_object(points):
     """Plots a basic object, assuming its convex and not too complex"""
