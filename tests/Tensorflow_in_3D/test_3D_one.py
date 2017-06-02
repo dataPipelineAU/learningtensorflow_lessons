e21ccda6d8ab25da6751183b2031e1c874343079
dealsy <deal.sy@hotmail.com>
2017-06-02 13:19:58 +1000

    added like  to remove invalid characters

--- tests/Tensorflow_in_3D/test_3D_one.py
+++ tests/Tensorflow_in_3D/test_3D_one.py
@@ -3,10 +3,15 @@ import numpy as np
 from matplotlib import cm
 import matplotlib.pyplot as plt
 from scipy.spatial import Delaunay
-import re
 
-re.sub('[^\w\-_\. ]', '_', filename)
 
+#!python3
+#coding: utf8
+symbols = '.'
+D = str.maketrans('','',symbols)
+with open('All12.txt','a',encoding='utf8') as t, open('All11.txt','r',encoding='utf8') as n:
+    for line in n:
+        t.write(line.translate(D))
 
 def plot_basic_object(points):
     """Plots a basic object, assuming its convex and not too complex"""
